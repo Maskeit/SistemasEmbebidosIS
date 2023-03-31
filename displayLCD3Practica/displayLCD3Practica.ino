47757c826e02e3cc42cf1661a8b4979d04c3a2a1
Maskeit <miguelagustin182@gmail.com>
2023-03-31 13:19:33 -0600

    practica 8

--- displayLCD3Practica/displayLCD3Practica.ino
+++ displayLCD3Practica/displayLCD3Practica.ino
@@ -106,16 +106,14 @@ void readNumber() {
   
 }
 
-void writeLongIntoEEPROM(int address, long number)
-{ 
+void writeLongIntoEEPROM(int address, long number){ 
   EEPROM.write(address, (number >> 24) & 0xFF);
   EEPROM.write(address + 1, (number >> 16) & 0xFF);
   EEPROM.write(address + 2, (number >> 8) & 0xFF);
   EEPROM.write(address + 3, number & 0xFF);
 }
 
-long readLongFromEEPROM(int address)
-{
+long readLongFromEEPROM(int address){
   return ((long)EEPROM.read(address) << 24) +
          ((long)EEPROM.read(address + 1) << 16) +
          ((long)EEPROM.read(address + 2) << 8) +
